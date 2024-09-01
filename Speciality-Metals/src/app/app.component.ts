2a3888f36f9e15c8bc4b9188d29a22d7bdf66821
u21509418 <127200948+Sammbecker@users.noreply.github.com>
2024-09-01 11:29:53 +0200

    x
    x

--- Speciality-Metals/src/app/app.component.ts
+++ Speciality-Metals/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- Speciality-Metals/src/app/app.routes.ts
+++ Speciality-Metals/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../Components/Login/Login/login/login.component';  // Adjust the path as necessary
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },  // Default route
+  { path: 'login', component: LoginComponent }           // Route to LoginComponent
+];
