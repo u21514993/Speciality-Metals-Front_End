aacd5d085aa72dcd2372e44eede67e7425922ec3
MandlaDev <u21657344@tuks.co.za>
2024-09-04 19:16:22 +0200

    g79g897g9

--- Speciality-Metals/src/app/shared/incoming.ts
+++ Speciality-Metals/src/app/shared/incoming.ts
@@ -1,12 +1,10 @@
-export interface incoming{
-    incomingID?:number;
-    incoming_Date?: Date;
-    gross_Weight?:number;
-    tare_Weight?:number;
-    net_Weight?:number;
-    gRV_Number?:number;
-    supplierID?: number;
-    productID?:number;
+export interface incoming {
+  incomingID?: number;
+  incoming_Date?: Date;
+  gross_Weight?: number;
+  tare_Weight?: number;
+  net_Weight?: number;
+  grV_Number?: string;
+  supplierID?: number;
+  productID?: number;
 }
-
-
