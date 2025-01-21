f21f0bb712388783b092efe1b97259731f08a0c4
u21514993 <u21514993@tuks.co.za>
2025-01-21 17:41:43 +0200

    ;l[]
    yt78

--- Speciality-Metals/src/app/shared/incoming.ts
+++ Speciality-Metals/src/app/shared/incoming.ts
@@ -1,13 +1,13 @@
 export interface incoming {
-  incomingID?: number;
-  incoming_Date?: Date;
-  gross_Weight?: number;
-  tare_Weight?: number;
-  net_Weight?: number;
-  supplierID?: number;
-  productID?: number;
-  employeeID?: number;
-  gRV_ID?: number;
-  comments?: string;
-  sundry_Note_ID: number;
+  incomingID: number;
+    incoming_Date: Date;  // Keep as Date type
+    gross_Weight: number;
+    tare_Weight: number;
+    net_Weight: number;
+    supplierID: number;
+    productID: number;
+    employeeID: number;
+    grV_ID: number;
+    comments?: string;
+    sundry_Note_ID: number;
 }
